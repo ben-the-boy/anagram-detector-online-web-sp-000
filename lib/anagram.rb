b9a7f4fb79fb6dee75e7d9bc72c2896dedff517d
Ben Hayes <b.t.hayes@comcast.net>
2021-01-04 22:39:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,6 @@ class Anagram
   def match(possible_anagrams)
     possible_anagrams.each do |word|
       sorted_word = word.split("").sort
-      binding.pry 
       if sorted_word == @word.split("").sort 
         word 
       end
